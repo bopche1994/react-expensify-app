cc59652194b5d99d36859a6a3d027d34b59c640c
Admin <bopche.vivek@gmail.com>
2020-07-28 10:55:21 +0530

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,6 +12,15 @@ import './styles/styles.scss';
   
 const store = configureStore();
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+    
+)
+ReactDOM.render(jsx, document.getElementById('app'));
+
+
 // console.log(store.getState());
 // store.subscribe(() => {
 //     const state = store.getState()
@@ -20,24 +29,24 @@ const store = configureStore();
 // })
 
 
-const expenseOne = store.dispatch(addExpense({description: 'Water Bill', amount: 10, createdAt: 1000}));
-const expenseTwo = store.dispatch(addExpense({description: 'Gas Bill', amount: 20, createdAt: 2000}));
-const expenseThree = store.dispatch(addExpense({description: 'Rent', amount: 30, createdAt: -2000}));
-const expenseFour = store.dispatch(addExpense({description: 'Cofffee', amount: 40, createdAt: -1000}));
+// const expenseOne = store.dispatch(addExpense({description: 'Water Bill', amount: 10, createdAt: 1000}));
+// const expenseTwo = store.dispatch(addExpense({description: 'Gas Bill', amount: 20, createdAt: 2000}));
+// const expenseThree = store.dispatch(addExpense({description: 'Rent', amount: 30, createdAt: -2000}));
+// const expenseFour = store.dispatch(addExpense({description: 'Cofffee', amount: 40, createdAt: -1000}));
 
 // store.dispatch(setTextFilter('water'))
 
 // setTimeout(() => {
 //     store.dispatch(setTextFilter('bill'))
 // }, 3000)
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+// const state = store.getState()
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 // console.log(visibleExpenses)
 
-const jsx = (
-    <Provider store={store}>
-        <AppRouter/>
-    </Provider>
+// const jsx = (
+//     <Provider store={store}>
+//         <AppRouter/>
+//     </Provider>
     
-)
-ReactDOM.render(jsx, document.getElementById('app'));
+// )
+// ReactDOM.render(jsx, document.getElementById('app'));
